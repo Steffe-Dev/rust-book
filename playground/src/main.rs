9223556234cf1f34b463be7a1385cad14f25991f
Frans <fstefanusbothma@gmail.com>
2025-02-20 20:33:41 +0200

    Add playground

---
+++ playground/src/main.rs
@@ -0,0 +1,20 @@
+fn main() {}
+
+pub fn remove_char(s: &str) -> String {
+    s.get(1..s.len() - 1).unwrap_or_default().to_string()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::remove_char;
+
+    #[test]
+    fn sample_cases() {
+        assert_eq!(remove_char("eloquent"), "loquen");
+        assert_eq!(remove_char("country"), "ountr");
+        assert_eq!(remove_char("person"), "erso");
+        assert_eq!(remove_char("place"), "lac");
+        assert_eq!(remove_char("ok"), "");
+        assert_eq!(remove_char("ooopsss"), "oopss");
+    }
+}
