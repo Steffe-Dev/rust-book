4fa3db8f45609d68a657f1d04eb211c96f3f0422
Frans Stefanus Bothma <fstefanusbothma@gmail.com>
2025-03-17 20:37:17 +0200

    End of Ch17

--- counting_async/src/main.rs
+++ counting_async/src/main.rs
@@ -1,9 +1,11 @@
-use counting_async::concurrency;
+use counting_async::{concurrency, streams};
 
 fn main() {
     // concurrency::counting();
     // concurrency::counting_join();
     // concurrency::channel();
     // concurrency::channel_dyn_futures();
-    concurrency::race();
+    // concurrency::race();
+    // streams::streams();
+    streams::messages();
 }
---
+++ counting_async/src/streams.rs
@@ -0,0 +1,69 @@
+use std::{pin::pin, time::Duration};
+
+use trpl::{ReceiverStream, Stream, StreamExt};
+
+pub fn streams() {
+    trpl::run(async {
+        let values = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+        let iter = values.iter().map(|n| n * 2);
+        let mut stream = trpl::stream_from_iter(iter);
+
+        while let Some(value) = stream.next().await {
+            println!("The value was: {value}");
+        }
+    })
+}
+
+pub fn messages() {
+    trpl::run(async {
+        let messages = get_messages().timeout(Duration::from_millis(200));
+        let intervals = get_intervals()
+            .map(|count| format!("Interval: {count}"))
+            .throttle(Duration::from_millis(100))
+            .timeout(Duration::from_secs(10));
+        let mut merged = pin!(messages.merge(intervals).take(20));
+
+        while let Some(message) = merged.next().await {
+            match message {
+                Ok(message) => println!("Got: {message}"),
+                Err(reason) => eprintln!("Problem: {reason:?}"),
+            }
+        }
+    })
+}
+
+fn get_messages() -> impl Stream<Item = String> {
+    let (tx, rx) = trpl::channel();
+
+    trpl::spawn_task(async move {
+        let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+        for (i, &message) in messages.iter().enumerate() {
+            let delay = if i % 2 == 0 { 100 } else { 300 };
+            trpl::sleep(Duration::from_millis(delay)).await;
+            if let Err(send_error) = tx.send(format!("Message: '{message}'")) {
+                eprintln!("Cannot send message '{message}': {send_error}");
+                break;
+            }
+        }
+    });
+
+    ReceiverStream::new(rx)
+}
+
+fn get_intervals() -> impl Stream<Item = u32> {
+    let (tx, rx) = trpl::channel();
+
+    trpl::spawn_task(async move {
+        let mut count = 0;
+        loop {
+            trpl::sleep(Duration::from_millis(1)).await;
+            count += 1;
+            if let Err(send_error) = tx.send(count) {
+                eprintln!("Could not send interval {count}: {send_error}");
+                break;
+            };
+        }
+    });
+
+    ReceiverStream::new(rx)
+}
